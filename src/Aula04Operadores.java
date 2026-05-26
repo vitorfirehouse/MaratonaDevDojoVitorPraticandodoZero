56ae08e68f679f61fbe8a298422fedba4c26cbff
vitorfirehouse <vitorcs1792@gmail.com>
2026-05-26 22:00:59 +0100

    Primeiro commit oficial

--- src/Aula04Operadores.java
+++ src/Aula04Operadores.java
@@ -9,10 +9,35 @@ public class Aula04Operadores {
         // O resultado de operações entre valores inteiros sempre resultará em um valor inteiro
         //Se uma variavel for inteiro e a outra double, o resultado não será inteiro
 
+        // OPERADORES LÓGICOS
+        int idade = 35;
+        float salario = 3500F;
+        boolean isDentroDalei = idade  > 30 && salario > 4612;
+        int idadePermitida = 18;
+        boolean temIngresso = true;
+        boolean podeEntrar = idadePermitida >= 18 && temIngresso;
 
-        boolean isDezMaiorQueVinte = 10 > 20;
-        boolean isDezMenorQueVinte = 10 < 20;
-        System.out.println(isDezMenorQueVinte);
+        // Média necessária para passar
+
+        double saldoNaconta = 500.00;
+        double valorDacompra = 120.00;
+        boolean cartaoInternacionalAceito = true;
+        boolean compraAutorizada = saldoNaconta >= valorDacompra && cartaoInternacionalAceito;
+
+//Verificando temperatura
+        int temp1 = 10;
+        int temp2 = 15;
+        int temp3 = 35;
+        boolean temperaturaPermitida = temp1 < 40 && temp2 < 40 && temp3 <40;
+
+// Para ganhar desconto no cinema, o cliente precisa cumprir duas condições: ter a idade menor que 12 anos E a carteirinha de estudante precisa ser verdadeira (boolean temCarteirinha = true;). Crie as variáveis para um cliente e monte o boolean ganhouDesconto.
+        int idadeParaCinema = 12;
+        boolean temCarteirnhadeEstudante = true;
+        boolean PodeEntrarse =  idadeParaCinema <= 12 && temCarteirnhadeEstudante;
+
+//
+        System.out.println(PodeEntrarse);
     }
+
 
 }
